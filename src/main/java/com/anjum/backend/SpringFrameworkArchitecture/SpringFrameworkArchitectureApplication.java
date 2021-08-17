756e83dbbc292fe91f7b649e28d56da16d7659ef
Abid Hussain Anjum <Aanjum@atheeb-ingr.com>
2021-08-17 12:46:38 +0300

    first commit

---
+++ src/main/java/com/anjum/backend/SpringFrameworkArchitecture/SpringFrameworkArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.anjum.backend.SpringFrameworkArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anjum/backend/SpringFrameworkArchitecture/SpringFrameworkArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjum.backend.SpringFrameworkArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
